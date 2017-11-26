f0f4fb5b6751923605b7ff1a836cd604e4cd41c1
Demian Arenas <arenasdemian99@gmail.com>
2017-11-26 01:58:26 -0600

    [update] add Sound  component with file into Pano

--- index.vr.js
+++ index.vr.js
@@ -5,6 +5,7 @@ import {
   Pano,
   Text,
   View,
+  Sound
 } from 'react-vr';
 import MainMenu from './Components/Scenes/MainMenu.js'
 
@@ -12,7 +13,13 @@ export default class OutdoorMovieTheater extends React.Component {
   render() {
     return (
       <View>
-        <Pano source={asset('fort-night.jpg')} />
+        <Pano source={asset('fort-night.jpg')}>
+        <Sound
+          volume={0.8}
+          loop = {true}
+          source={{mp3: asset('fort-night-atmosphere.mp3')}}
+        />
+        </Pano>
         <MainMenu />
       </View>
     );
